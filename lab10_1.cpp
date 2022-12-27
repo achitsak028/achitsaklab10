a0b8bbb0a72ff3d831c48ec68301deeb4beb725f
achitsak028 <patong028@gmail.com>
2022-12-27 16:47:08 +0700

    lab10_1 complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double loan,rate,payment;	
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,55 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	int year=1;
+	double inter=(loan*rate)/100;
+	double total=loan+inter;
+	double balance=total-payment;
+	if(balance<0){
+		if(payment>total){
+		payment=total;
+	}
+	balance =0;
+		cout << fixed << setprecision(2); 
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << inter;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << balance;
+	cout << "\n";
+	}
+	while(balance>0){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << inter;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << balance;
+	cout << "\n";
+	
+	year++;
+	loan=balance;
+	inter=(loan*rate)/100;
+	total=loan+inter;
+	if(payment>total){
+		payment=total;
+	}
+	
+	balance=total-payment;
+	if(balance==0){
+		cout << fixed << setprecision(2); 
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << inter;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << balance;
+	cout << "\n";
+	}
+	}	
+
 	
 	return 0;
 }
